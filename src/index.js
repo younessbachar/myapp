2d39cdda3d5672e651de7f743ff217b1127622c7
youness bachar khir <81125054+younessbachar@users.noreply.github.com>
2025-02-18 17:40:30 +0100

    commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import { RouterProvider , createBrowserRouter } from 'react-router-dom';
+import Home from './pages/home';
+import HTML from './pages/html';
+import Css from './pages/css';
+import Javascript from './pages/javascript';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    errorElement: <h1>404</h1>,
+  },
+  {
+    path: "/html",
+    element: <HTML />,
+    errorElement: <h1>404</h1>,
+  },
+  {
+    path: "/css",
+    element: <Css />,
+    errorElement: <h1>404</h1>,
+  },
+  {
+    path: "/javascript",
+    element: <Javascript />,
+    errorElement: <h1>404</h1>,
+  },
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
