552f90f2c04fabc69f7c52d2c0b1d57a683a7701
youness bachar khir <81125054+younessbachar@users.noreply.github.com>
2025-02-20 18:17:54 +0100

    commit

---
+++ src/components/footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+       <footer>
+        DDDesigned and developed by youness bachar-khir
+        <span>🧡</span>
+      </footer>
+  )
+}
+
+export default Footer
